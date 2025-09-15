f47915da60ac98ce45480b72024d82968ab26f0a
Davydov-Ruslan <113180875+Davydov-Ruslan@users.noreply.github.com>
2025-09-15 15:16:09 +0300

    Add LDPC (3/5) support for PSK16 simulations

--- ModelForLab1/Setup.m
+++ ModelForLab1/Setup.m
@@ -1,22 +1,22 @@
 
-% Без использования сверточного кода
-	Common.SaveFileName = 'QAM16_NoEncoder'; % Имя файла результата
-% Настройка параметров в classEncoder
-	Encoder.isTransparent = true; % без кодирования
-% Настройка параметров в classMapper
+% Р‘РµР· РёСЃРїРѕР»СЊР·РѕРІР°РЅРёСЏ СЃРІРµСЂС‚РѕС‡РЅРѕРіРѕ РєРѕРґР°
+	Common.SaveFileName = 'QAM16_NoEncoder'; % РРјСЏ С„Р°Р№Р»Р° СЂРµР·СѓР»СЊС‚Р°С‚Р°
+% РќР°СЃС‚СЂРѕР№РєР° РїР°СЂР°РјРµС‚СЂРѕРІ РІ classEncoder
+	Encoder.isTransparent = true; % Р±РµР· РєРѕРґРёСЂРѕРІР°РЅРёСЏ
+% РќР°СЃС‚СЂРѕР№РєР° РїР°СЂР°РјРµС‚СЂРѕРІ РІ classMapper
 	Mapper.isTransparent = false; 
 	Mapper.ModulationOrder = 16;
 	Mapper.DecisionMethod = 'Hard decision'; 
-% Настройка параметров в classChannel
+% РќР°СЃС‚СЂРѕР№РєР° РїР°СЂР°РјРµС‚СЂРѕРІ РІ classChannel
 	Channel.isTransparent = false;   
 % End of Params
 
-% ВАЖНО: Для каждого набора параметров нужно поставить % End of Params в конце
+% Р’РђР–РќРћ: Р”Р»СЏ РєР°Р¶РґРѕРіРѕ РЅР°Р±РѕСЂР° РїР°СЂР°РјРµС‚СЂРѕРІ РЅСѓР¶РЅРѕ РїРѕСЃС‚Р°РІРёС‚СЊ % End of Params РІ РєРѕРЅС†Рµ
 
 
-% С использованием сверточного кода
+% РЎ РёСЃРїРѕР»СЊР·РѕРІР°РЅРёРµРј СЃРІРµСЂС‚РѕС‡РЅРѕРіРѕ РєРѕРґР°
 
-	% QAM16 + CC + с жесткими решениями
+	% QAM16 + CC + СЃ Р¶РµСЃС‚РєРёРјРё СЂРµС€РµРЅРёСЏРјРё
 		Common.SaveFileName = 'QAM16_Encoder_HardDecision';
 		Encoder.isTransparent = false; 
 		Encoder.Trellis = poly2trellis(7,[171 133]); 
@@ -28,7 +28,7 @@
 		Channel.isTransparent = false;  
 		% End of Params
 	
-	% QAM16 + CC + с мягкими решениями
+	% QAM16 + CC + СЃ РјСЏРіРєРёРјРё СЂРµС€РµРЅРёСЏРјРё
 		Common.SaveFileName = 'QAM16_Encoder_SoftDecision_LLR'; 
 		Encoder.isTransparent = false; 
 		Encoder.Trellis = poly2trellis(7,[171 133]); 
@@ -40,7 +40,7 @@
 		Channel.isTransparent = false; 
 		% End of Params
 	
-	% QAM16 + CC + с мягкими решениями с аппросимацией
+	% QAM16 + CC + СЃ РјСЏРіРєРёРјРё СЂРµС€РµРЅРёСЏРјРё СЃ Р°РїРїСЂРѕСЃРёРјР°С†РёРµР№
 		
 		Common.SaveFileName = 'QAM16_Encoder_SoftDecision_ALLR'; 
 		Encoder.isTransparent = false; 
@@ -49,6 +49,18 @@
 		Encoder.DecodingType ='unquant'; 
 		Mapper.isTransparent = false; 
 		Mapper.ModulationOrder = 16; 
-		Mapper.DecisionMethod = 'Approximate log-likelihood ratio'; 
-		Channel.isTransparent = false;  
-		% End of Params
+                Mapper.DecisionMethod = 'Approximate log-likelihood ratio';
+                Channel.isTransparent = false;
+                % End of Params
+
+% PSK16 + LDPC (rate 3/5) + soft decisions
+    Common.SaveFileName = 'PSK16_LDPC_R3_5';
+    Encoder.isTransparent = false;
+    Encoder.TypeEncoder   = 'LDPC';
+    Encoder.CodeRate      = '3/5';
+    Mapper.isTransparent  = false;
+    Mapper.Type           = 'PSK';
+    Mapper.ModulationOrder= 16;
+    Mapper.DecisionMethod = 'Log-likelihood ratio';
+    Channel.isTransparent = false;
+    % End of Params
